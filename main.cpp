fc57dfae6c6a9991a91531897e304a227de12814
George <george@chameleon.ad>
2017-05-17 12:20:07 +0100

    fixed node shared pointer creation problem... do some tests to see if multithreading is actually working

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <node.h>
+#include <iostream>
+
+bool nodeCreateAndModifyTest() {
+	auto myNode = Node<int>::create(20, {});
+	std::cout << myNode->load()->payload << "\n";
+	myNode->load()->modify(50, {});
+	std::cout << myNode->load()->payload << "\n";
+	return true;
+}
 
 //Used for testing only at this point
 int main(int argc, char *argv[])
 {
-	auto myNode = Node<int>::create();
+	nodeCreateAndModifyTest();
 	return 1;
 }
+
+
--- node.h
+++ node.h
@@ -7,22 +7,28 @@
 template<typename InternalDataType>
 class Node {
 public:
+	//typedefs
+	typedef std::atomic<Node<InternalDataType>*> NodeMetadata;
+
 	//Containers
-	std::weak_ptr<std::atomic<Node<InternalDataType>*>> self{};
-	std::list<std::shared_ptr<std::atomic<Node<InternalDataType>*>>> links{};
+	std::weak_ptr<NodeMetadata> self{};
+	std::list<std::shared_ptr<NodeMetadata>> links{};
 	InternalDataType payload;
 	//Create
-	static std::shared_ptr<std::atomic<Node<InternalDataType>*>> create() {
-		std::atomic<Node<InternalDataType>*> nodeUniquePointer;
-		nodeUniquePointer.store(new Node<InternalDataType>{});
-		std::shared_ptr<std::atomic<Node<InternalDataType>*>> sharedPointerToMetadata = std::make_shared<std::atomic<Node<InternalDataType>*>>(nodeUniquePointer);
-		(*sharedPointerToMetadata).load()->self = sharedPointerToMetadata;
+	static std::shared_ptr<std::atomic<Node<InternalDataType>*>> create(const InternalDataType payload, const std::list<std::shared_ptr<NodeMetadata>> pointers) {
+
+		std::shared_ptr<NodeMetadata> sharedPointerToMetadata = std::make_shared<NodeMetadata>(new Node<InternalDataType>{payload, pointers});
+
+		NodeMetadata nodeUniquePointer{};
+		nodeUniquePointer.store(*sharedPointerToMetadata.get());
+		nodeUniquePointer.load()->self = sharedPointerToMetadata;
 		return sharedPointerToMetadata;
 	}
 	//Modify <-- Thread safe
-	void modify(const Node & newNode) {
-		(*(self)).exchange(new Node(newNode));
+	void modify(const InternalDataType payload, const std::list<std::shared_ptr<NodeMetadata>> pointers) {
+		self.lock()->exchange(new Node(payload, pointers));
 	}
 private:
 	Node() {}
+	Node(InternalDataType payload, std::list<std::shared_ptr<NodeMetadata>> pointers): payload(payload), links(links) {}
 };
