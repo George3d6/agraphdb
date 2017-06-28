de9938394ef5a563a14d3202e71d1dea6469ebe4
George Hosu <george.hous@protonmail.com>
2017-06-28 10:22:00 +0000

    going down the lockfree way

--- datatypes/json.h
+++ datatypes/json.h
@@ -1,28 +1,61 @@
 #pragma once
 
+#include <memory>
+
+#include "libs/rapidjson/document.h"
+#include "libs/rapidjson/writer.h"
+#include "libs/rapidjson/stringbuffer.h"
+
 namespace data {
 class Json {
 public:
     //members
-    std::string content;
+    rapidjson::Document content;
 
     //mandatory methods
     std::string serialize() const noexcept {
-        return "!!PANIC!!  ";
+        rapidjson::StringBuffer buffer;
+        rapidjson::Writer writer(buffer);
+        content.Accept(writer);
+        return buffer.GetString();
     }
 
     //Lifetime management
-    template <class JsonType>
-    Json(JsonType json) noexcept : content(json) {}
+    template <class StringType>
+    Json(StringType json) {
+        content.Parse(json);
+    }
     Json(const Json & json):content(json.content) {}
 
-    template <class JsonType>
-    Json& operator=(JsonType & new_content) {
-        this->content = new_content;
+    template <class StringType>
+    Json& operator=(StringType & new_content) {
+        this->content.Parse(new_content);
         return *this;
     }
     //Mandatory:
     Json(std::string serialization) noexcept : {
+        content.Parse(json);
+    }
+
+    //GetValue for various types
+    template<template <class...> class ValueType>
+    std::unique_ptr<ValueType> get_value(std::string member_path) {
+        if(!content.HasMember(member_path)) {
+            return nullptr;
+        }
+
+        if(typeid(ValueType) == typeid(int)) {
+           return std::make_unique<std::string>{content[member_path].GetInt()};
+        }
+        if(typeid(ValueType) == typeid(double) || typeid(ValueType) == typeid(float)) {
+           return std::make_unique<std::string>{content[member_path].GetDouble()};
+        }
+        if(typeid(ValueType) == typeid(std::string)) {
+           return std::make_unique<std::string>{content[member_path].GetString()};
+        }
+        if(typeid(ValueType) == typeid(std::string)) {
+           return std::make_unique<std::string>{content[member_path].GetString()};
+        }
 
     }
 };
